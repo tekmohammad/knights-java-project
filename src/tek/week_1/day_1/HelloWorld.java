ccadd7be5da249335132ce30919b9a13935314e8
Mohammad Shokriyan <git@tekshool.us>
2023-06-27 18:55:52 -0400

    Week 1 Day 1

---
+++ src/tek/week_1/day_1/HelloWorld.java
@@ -0,0 +1,10 @@
+package tek.week_1.day_1;
+
+public class HelloWorld {
+	
+	// main method.
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+
+}
---
+++ src/tek/week_1/day_1/HumanObject.java
@@ -0,0 +1,21 @@
+package tek.week_1.day_1;
+
+import java.util.Date;
+
+public class HumanObject {
+	
+	//Properties. 
+	//Variables. 
+	String name = "Mohammad"; 
+	Date dateOfBirth; 
+	double height = 165.8; 
+	int weight = 74; 
+	
+	
+	//behaviors
+	//Methods
+	public void canWalk() {}
+	
+	public void canTalk() {}
+
+}
